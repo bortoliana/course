cf04654af20cb7dab70007a7554653e7d137fa75
ana <analeticiadebortoli@gmail.com>
2022-12-03 11:37:09 -0300

    OrderStatus enum

--- src/main/java/com/bortoli/course/entities/Order.java
+++ src/main/java/com/bortoli/course/entities/Order.java
@@ -1,14 +1,14 @@
 package com.bortoli.course.entities;
 
+import com.bortoli.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
 
 @Getter
 @Setter
@@ -23,24 +23,14 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order(){
     }
-    public Order(Long id, Instant moment, User client){
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Order order)) return false;
-        return getId().equals(order.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client){
     }
 }
